e3d5e8f59469110736aa419d11b26d5a1e0bfd6f
mokuzaru <kenny.otoya@pucp.pe>
2024-03-30 23:14:40 -0500

    finish counter app

--- App.tsx
+++ App.tsx
@@ -2,13 +2,19 @@ import { SafeAreaView } from "react-native"
 import { HelloworldScreen } from "./src/presentation/screens/HelloworldScreen"
 import { CounterScreen } from "./src/presentation/screens/CounterScreen"
 import { PaperProvider } from "react-native-paper"
+import { CounterM3Screen } from "./src/presentation/screens/CounterM3Screen"
 
+import IonIcon from 'react-native-vector-icons/Ionicons'
 
 export const App = () => {
   return (
-    <PaperProvider>
+    <PaperProvider
+      settings={{
+        icon: (props) => <IonIcon { ...props } />
+      }}
+    >
       <SafeAreaView style={{flex:1}}>
-        <CounterScreen/>
+        <CounterM3Screen/>
       </SafeAreaView>
     </PaperProvider>
     
--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -1,20 +1,38 @@
 import { useState } from "react"
-import { Text, View } from "react-native"
+import { Platform, StyleSheet, Text, View } from "react-native"
 import { globalStyles } from "../theme/global.styles"
+import { FAB } from "react-native-paper"
 
+import Icon from 'react-native-vector-icons/Ionicons'
 
-export const CounterScreen = () => {
+export const CounterM3Screen = () => {
 
     const [count, setcount] = useState(10)
 
 
     return (
-        <View style= { globalStyles.container }>
+        <View style= { globalStyles.centerContainer }>
             <Text style= { globalStyles.title }>{count}</Text>
           
-            
+            <Icon name="game-controller-outline" size={25}/>
+            <FAB                 
+                onPress={() => setcount(count+1)}
+                onLongPress={(() => setcount(0))}
+                style= {styles.fab}
+                //icon={() => <Icon name="add-outline" size={25} />}
+                icon="add"
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create ({ 
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: Platform.OS === 'android' ? 15 : 0,
+    }
+})
 
 
